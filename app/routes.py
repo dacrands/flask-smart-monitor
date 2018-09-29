40d629ba079ec6a6fd35888c7a14cbae76544585
da crands <davecrands@gmail.com>
2018-09-29 15:03:54 -0400

    Flash error styling

--- app/routes.py
+++ app/routes.py
@@ -37,6 +37,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            print("==============ASD===============")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
