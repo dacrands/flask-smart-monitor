529aef5ee3eaa580f319474f660df142a5ae4740
da crands <davecrands@gmail.com>
2019-11-10 19:05:56 -0500

    Add create_app to app __init__

--- app/__init__.py
+++ app/__init__.py
@@ -8,41 +8,53 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login = LoginManager()
 login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    bcrypt.init_app(app)
 
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler(
-        'logs/app.log',
-        maxBytes=10240,
-        backupCount=10
-    )
-    file_handler.setFormatter(
-        logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/app.log',
+            maxBytes=10240,
+            backupCount=10
         )
-    )
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+        file_handler.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+            )
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
-    file_handler.setLevel(logging.INFO)
-    app.logger.info('ToViewIt')
+        file_handler.setLevel(logging.INFO)
+        app.logger.info('ToViewIt')
+
+    return app
+
 
 from app import models
